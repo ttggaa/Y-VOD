335eb988fede7c2a7f64cffca1305825ed7f314f
iROCKBUNNY <irockbunny@outlook.com>
2018-10-09 14:38:35 +0800

    Update

--- app/static/assets/js/common.js
+++ app/static/assets/js/common.js
@@ -1,1 +1,6 @@
 // common.js
+
+// footer copyright
+$('#copyright').html(function () {
+    return '<i class="copyright outline icon"></i> 2011-' +  new Date().getFullYear() + ' 北京云英一语教育咨询有限公司 版权所有';
+});
--- app/status/views.py
+++ app/status/views.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
 
 '''app/status/views.py'''
+
+from htmlmin import minify
+from flask import render_template, redirect, request, url_for, abort, current_app
+from flask_login import login_required, current_user
+from . import status
+from ..decorators import permission_required
+
+
+@status.route('/')
+@login_required
+@permission_required('管理')
+def home():
+    '''status.home()'''
+    return minify(render_template('status/home.html'))
